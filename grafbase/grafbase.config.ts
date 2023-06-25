a98a4d0c1b7964b63bdd3e526fa821927345f6c9
Jatin <jatinsharma13arril2002@gmail.com>
2023-06-25 15:10:52 +0530

    first commit

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,33 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+const User = g.model('User', {
+  name: g.string().length({min:3, max:20}),
+  email:g.string().unique(),
+  avatarUrl:g.url(),
+  description:g.string(),
+  githubUrl:g.url().optional(),
+  linkedinUrl:g.url().optional(),
+  projects:g.relation(() => Project).list().optional(),
+})
+
+const Project = g.model('Prject',{
+  title:g.string().length({min:3}),
+  description:g.string().optional(),
+  image:g.url(),
+  liveSiteUrl:g.url(),
+  githubUrl:g.url(),
+  category:g.string().search(),
+  createdBy:g.relation(()=> User)
+})
+
+export default config({
+  schema: g
+  // Integrate Auth
+  // https://grafbase.com/docs/auth
+  // auth: {
+  //   providers: [authProvider],
+  //   rules: (rules) => {
+  //     rules.private()
+  //   }
+  // }
+})
