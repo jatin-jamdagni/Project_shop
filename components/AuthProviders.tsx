f4894a746c54488218684d05a95135412e84094d
Jatin <jatinsharma13arril2002@gmail.com>
2023-06-25 17:31:33 +0530

    added auth

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { getProviders, signIn } from 'next-auth/react';
+import React, { useEffect, useState } from 'react'
+
+// import Button from './Button';
+
+type Provider = {
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
+
 
 const AuthProviders = () => {
-    return (
-        <div>AuthProviders</div>
-    )
+    const [providers, setProviders] = useState<Providers | null>(null);
+
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const res = await getProviders();
+            console.log(res)
+            setProviders(res);
+        }
+
+        fetchProviders();
+    }, []);
+    const [provider, setProvider] = useState<Providers | null>(null);
+
+    if (provider) {
+        return (
+            <div>
+                {Object.values(provider).map((provider: Provider, index) => (
+                    <button key={index}>{provider.id}</button>
+                ))}
+            </div>
+        )
+    }
 }
 
 export default AuthProviders
--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -22,12 +22,4 @@ const Project = g.model('Prject',{
 
 export default config({
   schema: g
-  // Integrate Auth
-  // https://grafbase.com/docs/auth
-  // auth: {
-  //   providers: [authProvider],
-  //   rules: (rules) => {
-  //     rules.private()
-  //   }
-  // }
 })
