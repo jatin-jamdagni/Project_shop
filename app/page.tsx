0332139354404d89a5013122aa2d0c51191b4e87
Jatin <jatinsharma13arril2002@gmail.com>
2023-06-28 17:51:00 +0530

    added uploades sucessfull wiht external links in next.confi.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,32 @@
-const Home = () => {
+import { ProjectInterface } from "@/common.types";
+import { fetchAllProjects } from "@/lib/actions";
+
+type ProjectSearch = {
+    projectSearch: {
+        edges: { node: ProjectInterface }[],
+        pageInfo: {
+            hasPreviousPage: boolean;
+            hasNextPage: boolean;
+            startCursor: string;
+            endCursor: string;
+        }
+
+    }
+}
+const Home = async () => {
+
+    const data = await fetchAllProjects() as ProjectSearch;
+
+    const projectToDisplay = data?.projectSearch?.edges || [];
+    if (projectToDisplay.length === 0) {
+        return (
+            <section className="flexStart flex-col paddings">
+                categories
+
+                <p className="no-result-text text-center">No projects found, go create some first.</p>
+            </section>
+        )
+    }
     return (
         <section className="flex-start flex-col paddings mb-16">
             <h1>categories</h1>
