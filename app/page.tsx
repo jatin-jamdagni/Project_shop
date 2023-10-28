514e4388f04b53fc16f64513bb9d30a7a42d6707
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2023-10-28 10:07:47 +0530

    fiexed some chANGES

--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,79 @@
-import { ProjectInterface } from "@/common.types";
-import Categories from "@/components/Categories";
 import LoadMore from "@/components/LoadMore";
-import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
+import Categories from "@/components/Categories";
+import { ProjectInterface } from "@/common.types";
+import ProjectCard from "@/components/ProjectCard";
 
 type ProjectSearch = {
-    projectSearch: {
-        edges: { node: ProjectInterface }[],
-        pageInfo: {
-            hasPreviousPage: boolean;
-            hasNextPage: boolean;
-            startCursor: string;
-            endCursor: string;
-        }
-
-    }
-}
+  projectSearch: {
+    edges: { node: ProjectInterface }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
+    };
+  };
+};
 
 type SearchParams = {
-    category?: string | null;
-
-}
+  category?: string;
+  endcursor?: string;
+};
 
 type Props = {
-    searchParams: SearchParams;
-}
-const Home = async ({ searchParams: { category } }: Props) => {
+  searchParams: SearchParams;
+};
 
-    const data = await fetchAllProjects(category) as ProjectSearch;
+export const dynamic = "force-dynamic"; // force dynamic for this page so that it is always regenerated when a new project is created
+export const dynamicParams = true;
+export const revalidate = 0;
 
-    const projectToDisplay = data?.projectSearch?.edges || [];
-    if (projectToDisplay.length === 0) {
-        return (
-            <section className="flexStart flex-col paddings">
-                categories
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+  const data = (await fetchAllProjects(category, endcursor)) as ProjectSearch;
 
-                <p className="no-result-text text-center">No projects found, go create some first.</p>
-            </section>
-        )
-    }
+  const projectsToDisplay = data?.projectSearch?.edges || [];
 
-    const pagination = data?.projectSearch?.pageInfo;
+  if (projectsToDisplay.length === 0) {
     return (
-        <section className="flex-start flex-col paddings mb-16">
-            <Categories />
-            <section className="projects-grid">
-                {projectToDisplay.map(({ node }: { node: ProjectInterface }) => (
-                    <ProjectCard
-                        key={node?.id}
-                        id={node?.id}
-                        image={node?.image}
-                        title={node?.title}
-                        name={node?.createdBy?.name}
-                        avatarUrl={node?.createdBy?.avatarUrl}
-                        userId={node?.createdBy?.id} />
-                ))}
-            </section>
-            <LoadMore
-                startCursor={pagination?.startCursor}
-                endCursor={pagination?.endCursor}
-                hasPreviousPage={pagination?.hasPreviousPage}
-                hasNextPage={pagination?.hasNextPage}
+      <section className="flexStart flex-col paddings">
+        <Categories />
 
-            />
-        </section>
-    )
-}
+        <p className="no-result-text text-center">
+          No projects found, go create some first.
+        </p>
+      </section>
+    );
+  }
+
+  const pagination = data?.projectSearch?.pageInfo;
+
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <Categories />
+
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={node?.id}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy?.name}
+            avatarUrl={node?.createdBy?.avatarUrl}
+            userId={node?.createdBy?.id}
+          />
+        ))}
+      </section>
+
+      <LoadMore
+        startCursor={pagination.startCursor}
+        endCursor={pagination.endCursor}
+        hasPreviousPage={pagination.hasPreviousPage}
+        hasNextPage={pagination.hasNextPage}
+      />
+    </section>
+  );
+};
 
 export default Home;
