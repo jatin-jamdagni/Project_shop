d54f7b12964f09bae98d597407ca007b90f271a7
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2023-10-28 11:00:30 +0530

    added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import LoadMore from "@/components/LoadMore";
-import { fetchAllProjects } from "@/lib/actions";
-import Categories from "@/components/Categories";
 import { ProjectInterface } from "@/common.types";
+import Categories from "@/components/Categories";
+import LoadMore from "@/components/LoadMore";
 import ProjectCard from "@/components/ProjectCard";
+import { fetchAllProjects } from "@/lib/actions";
 
 type ProjectSearch = {
   projectSearch: {
@@ -17,28 +17,20 @@ type ProjectSearch = {
 };
 
 type SearchParams = {
-  category?: string;
-  endcursor?: string;
+  category?: string | null;
 };
 
 type Props = {
   searchParams: SearchParams;
 };
+const Home = async ({ searchParams: { category } }: Props) => {
+  const data = (await fetchAllProjects(category)) as ProjectSearch;
 
-export const dynamic = "force-dynamic"; // force dynamic for this page so that it is always regenerated when a new project is created
-export const dynamicParams = true;
-export const revalidate = 0;
-
-const Home = async ({ searchParams: { category, endcursor } }: Props) => {
-  const data = (await fetchAllProjects(category, endcursor)) as ProjectSearch;
-
-  const projectsToDisplay = data?.projectSearch?.edges || [];
-
-  if (projectsToDisplay.length === 0) {
+  const projectToDisplay = data?.projectSearch?.edges || [];
+  if (projectToDisplay.length === 0) {
     return (
       <section className="flexStart flex-col paddings">
-        <Categories />
-
+        categories
         <p className="no-result-text text-center">
           No projects found, go create some first.
         </p>
@@ -47,13 +39,11 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
   }
 
   const pagination = data?.projectSearch?.pageInfo;
-
   return (
     <section className="flex-start flex-col paddings mb-16">
       <Categories />
-
       <section className="projects-grid">
-        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+        {projectToDisplay.map(({ node }: { node: ProjectInterface }) => (
           <ProjectCard
             key={node?.id}
             id={node?.id}
@@ -65,12 +55,11 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
           />
         ))}
       </section>
-
       <LoadMore
-        startCursor={pagination.startCursor}
-        endCursor={pagination.endCursor}
-        hasPreviousPage={pagination.hasPreviousPage}
-        hasNextPage={pagination.hasNextPage}
+        startCursor={pagination?.startCursor}
+        endCursor={pagination?.endCursor}
+        hasPreviousPage={pagination?.hasPreviousPage}
+        hasNextPage={pagination?.hasNextPage}
       />
     </section>
   );
